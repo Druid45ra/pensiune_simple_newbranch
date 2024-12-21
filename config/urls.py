c0a368495a289c160a44d403dd5feba4e56636e6
Radu <vancaradu@gmail.com>
2024-12-21 16:56:26 +0200

    First change

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,17 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from django.contrib import admin
+from pensiune import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("", views.HomeView.as_view(), name="home"),
     path("camere/", views.ListareCamereView.as_view(), name="camere"),
     path("rezervare/", views.RezervareCreateView.as_view(), name="rezervare"),
     path("contact/", views.ContactCreateView.as_view(), name="contact"),
     path("despre/", views.DespreNoiView.as_view(), name="despre"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
